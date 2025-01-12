61382d3491480bfbe02670e6666094ff4dcd91c4
Victor Georges <victorgeorges54@gmail.com>
2025-01-12 21:34:06 +0100

    :sparkles: Ajout de la qualité de l'air

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
 import { getGeolocalisation } from "./js/geolocalisation.js";
 import { getMeteo } from "./js/meteo.js";
 import { getVelos } from "./js/velo.js";
+import { getAir } from "./js/air.js";
 
 // Récupération de toutes les données
 const geolocalisation = await getGeolocalisation();
 await getMeteo(geolocalisation.latitude, geolocalisation.longitude, geolocalisation.ville);
 await getVelos(geolocalisation.latitude, geolocalisation.longitude);
+await getAir();
